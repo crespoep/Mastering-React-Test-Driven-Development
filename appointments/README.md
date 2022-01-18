c3fc9ecf9260ee63a2e9c1d867e03bcb728feaf8
Ezequiel <crespo.ep@gmail.com>
2022-01-18 14:03:16 -0300

    first chapter ended, with all exercises

---
+++ appointments/README.md
@@ -0,0 +1,10 @@
+To run tests:
+
+$ npm test
+or
+$ npm run test-watch 
+
+To run webpack:
+$ npm run build
+
+To open the app you should open de index.html file in the browser
--- appointments/test/AppointmentsDayView.test.js
+++ appointments/test/AppointmentsDayView.test.js
@@ -4,11 +4,11 @@ import ReactTestUtils from 'react-dom/test-utils'
 import { 
     Appointment,
     AppointmentsDayView 
-} from '../src/Appointment'
+} from '../src/AppointmentsDayView'
 
 describe('Appointment', () => {
 
-    let customer
+    let customer = {}
     let container
 
     beforeEach(() => {
@@ -16,6 +16,13 @@ describe('Appointment', () => {
     })
 
     const render = component => ReactDOM.render(component, container)
+
+    const appointmentTable = () => container.querySelector('div#appointmentView > table')
+
+    it('renders a table for the details', () => {
+        render(<Appointment customer={customer}/>)
+        expect(container.querySelector('div#appointmentView')).not.toBeNull()
+    })
 
     it('renders the customer first name', () => {
         customer = { firstName: 'Ashley' }
@@ -27,6 +34,56 @@ describe('Appointment', () => {
         customer = { firstName: 'Jordan' }
         render(<Appointment customer={customer} />, container)
         expect(container.textContent).toMatch('Jordan')
+    })
+
+    it('renders the customer last name', () => {
+        customer = { lastName: 'Dorne' }
+        render(<Appointment customer={customer} />)
+        expect(container.textContent).toMatch('Dorne')
+    })
+
+    it('renders the customer phone number', () => {
+        customer = { phoneNumber: '555-5555-555'}
+        render(<Appointment customer={customer} />)
+        expect(appointmentTable().textContent).toMatch('555-5555-555')
+    })
+
+    it('renders the stylist name', () => {
+        render(<Appointment stylist="Daniel" customer={customer}/>)
+        expect(appointmentTable().textContent).toMatch('Daniel')
+    })
+
+    it('renders another stylist name', () => {
+        render(<Appointment customer={customer} stylist="Jo" />);
+        expect(appointmentTable().textContent).toMatch('Jo');
+      });
+
+    it('renders the salon service', () => {
+        render(<Appointment customer={customer} service="HairCut" />)
+        expect(appointmentTable().textContent).toMatch('HairCut')
+    })
+
+    it('renders another salon service', () => {
+        render(<Appointment customer={customer} service="Blow-dry" />);
+        expect(appointmentTable().textContent).toMatch('Blow-dry');
+      });
+
+    it('renders notes about the appointment', () => {
+        render(<Appointment customer={customer} notes="With care please!" />)
+        expect(appointmentTable().textContent).toMatch('With care please!')
+    })
+
+    it('renders other appointment notes', () => {
+        render(<Appointment customer={customer} notes="def" />);
+        expect(appointmentTable().textContent).toMatch('def');
+      });
+
+    it('renders a heading for the appointments table', () => {
+        const today = new Date()
+        const startsAt = today.setHours(9, 0, 0)
+        const heading = "Today's appointment at 09:00"
+        render(<Appointment customer={customer} startsAt={startsAt} />)
+        expect(container.textContent).toMatch("Today's appointment at 09:00")
     })
 })
 
@@ -91,4 +148,17 @@ describe('AppointmentsDayView', () => {
         ReactTestUtils.Simulate.click(button)
         expect(container.textContent).toMatch('Jordan')
     })
+
+    it('adds toggled class to button when selected', () => {
+        render(<AppointmentsDayView appointments={appointments} />)
+        const button = container.querySelectorAll('button')[1]
+        ReactTestUtils.Simulate.click(button)
+        expect(button.className).toMatch('toggled')
+    })
+
+    it('does not add toggled class if button is not selected', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        const button = container.querySelectorAll('button')[1];
+        expect(button.className).not.toMatch('toggled');
+      })
 })
