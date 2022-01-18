5a5524a44142cd44a3a6b663bd1aaa9309458a68
Ezequiel <crespo.ep@gmail.com>
2022-01-18 18:29:19 -0300

    adding a couple of tests, for the first name field and the its label

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -1,3 +1,15 @@
 import React from "react"
 
-export const CustomerForm = () => <form id="customer"></form>
+
+
+export const CustomerForm = ({firstName}) => 
+<form id="customer">
+  <label htmlFor="firstName">First name</label>
+  <input
+      id="firstName"
+      type="text"
+      name="firstName"
+      value={firstName}
+      readOnly
+  />
+</form>
--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -3,18 +3,52 @@ import { createContainer } from './domManipulators'
 import { CustomerForm } from '../src/CustomerForm'
 
 describe('CustomerForm', () => {
-    let render, container
+  let render, container
 
-    const form = id => container.querySelector(`form[id="${id}"]`)
+  const form = id => container.querySelector(`form[id="${id}"]`)
 
-    beforeEach(() => {
-        ({ render, container } = createContainer())
-    })
+  const firstNameField = () => form('customer').elements.firstName
 
-    it('renders a form', () => {
-        render(<CustomerForm />)
-        expect(form('customer')).not.toBeNull()
-    })
+  const labelFor = formElement => container.querySelector(`label[for="${formElement}"]`)
 
-    
+  const expectToBeInputFieldOfTypeText = formElement => {
+    expect(formElement).not.toBeUndefined()
+    expect(formElement.tagName).toEqual('INPUT')
+    expect(formElement.type).toEqual('text')
+  }
+
+  beforeEach(() => {
+      ({ render, container } = createContainer())
+  })
+
+  it('renders a form', () => {
+      render(<CustomerForm />)
+      expect(form('customer')).not.toBeNull()
+  })
+
+  it('renders the first name field as text box', () => {
+      render(<CustomerForm />)
+      expectToBeInputFieldOfTypeText(firstNameField())
+  })
+
+  it('includes the existing value for the first name', () => {
+    render(<CustomerForm firstName="Ashley" />)
+    expect(firstNameField().value).toEqual('Ashley')
+  })
+
+  it('renders a label field for the first name field', () => {
+    render(<CustomerForm />)
+    expect(labelFor('firstName')).not.toBeNull()
+    expect(labelFor('firstName').textContent).toEqual('First name')
+  })
+
+  it('assigns an id to the first name field', () => {
+    render(<CustomerForm />)
+    expect(firstNameField().id).toEqual('firstName')
+  })
+
+  it('assings an id that matches the label id to the first name field', () => {
+    render(<CustomerForm />)
+    expect(firstNameField().id).toEqual(labelFor('firstName').htmlFor)
+  })
 })
