3333283b2ccc75cb84338ffb9038e127e3e3bfa6
Ezequiel <crespo.ep@gmail.com>
2022-01-20 12:30:07 -0300

    renders the header of the table with the hours available in the appointment form

--- appointments/src/AppointmentForm.js
+++ appointments/src/AppointmentForm.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react"
 
-export const AppointmentForm = ({selectableServices, service, onSubmit}) => {
+export const AppointmentForm = ({
+  selectableServices,
+  service,
+  onSubmit,
+  salonOpensAt,
+  salonClosesAt
+}) => {
 
   const [appointment, setAppointment] = useState({service})
 
@@ -29,12 +35,48 @@ export const AppointmentForm = ({selectableServices, service, onSubmit}) => {
         }
 
       </select>
-      <TimeSlotTable />
+      <TimeSlotTable 
+        salonOpensAt={salonOpensAt}
+        salonClosesAt={salonClosesAt}
+      />
     </form>
   )
 }
 
-const TimeSlotTable = () => <table id="time-slots"></table>
+const dailyTimeSlots = (salonOpensAt, salonClosesAt) => {
+  const totalSlots = (salonClosesAt - salonOpensAt) * 2
+  const startTime = new Date().setHours(salonOpensAt, 0, 0, 0)
+  // converts 30 minutes to miliseconds
+  const increment = 30 * 60 * 1000
+  return Array(totalSlots)
+    .fill([startTime]) // [[100], [100], [100], [100]]
+    .reduce((acc, _, i) => acc.concat([startTime + (i * increment)])
+    )
+  }
+
+const toTimeValue = timestamp => 
+  new Date(timestamp).toTimeString().substring(0, 5)
+
+const TimeSlotTable = ({
+  salonOpensAt,
+  salonClosesAt
+}) => {
+  const timeSlots = dailyTimeSlots(salonOpensAt, salonClosesAt)
+  
+  return (
+    <table id="time-slots">
+      <tbody>
+        {
+          timeSlots.map(timeSlot => 
+            <tr key={timeSlot}>
+              <th>{toTimeValue(timeSlot)}</th>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+  )
+}
 
 AppointmentForm.defaultProps = {
   selectableServices: [
@@ -44,5 +86,7 @@ AppointmentForm.defaultProps = {
     'Beard trim',
     'Cut & beard trim',
     'Extensions'
-  ]
+  ],
+  salonOpensAt: 9,
+  salonClosesAt: 19
 }
--- appointments/test/AppointmentForm.test.js
+++ appointments/test/AppointmentForm.test.js
@@ -104,14 +104,25 @@ describe('AppointmentForm', () => {
     })
   })
 
+  const timeSlotTable = () => container.querySelector('table#time-slots')
+
   describe('time slot table', () => {
     it('renders a table for time slots', () => {
       render(<AppointmentForm />)
-      expect(
-        container.querySelector('table#time-slots')
-      ).not.toBeNull()
+      expect(timeSlotTable()).not.toBeNull()
+    })
+    it('renders a time slot for every half an hour between open and cloase times', () => { 
+      render(
+        <AppointmentForm salonOpensAt={9} salonClosesAt={11} />
+      )
+      const timesOfDay = timeSlotTable().querySelectorAll(
+        'tbody >* th'
+      )
+      expect(timesOfDay).toHaveLength(4)
+      expect(timesOfDay[0].textContent).toEqual('09:00')
+      expect(timesOfDay[1].textContent).toEqual('09:30')
+      expect(timesOfDay[3].textContent).toEqual('10:30')
     })
 
-    
   })
 })
