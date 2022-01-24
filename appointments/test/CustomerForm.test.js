8686db9782861c5371b804efeca832727b7cb1a9
Ezequiel <crespo.ep@gmail.com>
2022-01-24 18:31:21 -0300

    created my first custom matcher

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -24,6 +24,18 @@ describe('CustomerForm', () => {
     }
   }
 
+  expect.extend({
+    toHaveBeenCalled(received) {
+      if (received.receivedArguments() === undefined) {
+        return {
+          pass: false,
+          message: () => 'Spy was not called.'
+        }
+      }
+      return { pass: true, message: () => 'Spy was called.'}
+    }
+  })
+
   it('renders a form', () => {
     render(<CustomerForm />);
     expect(form('customer')).not.toBeNull();
@@ -79,7 +91,7 @@ describe('CustomerForm', () => {
         />
       );
       ReactTestUtils.Simulate.submit(form('customer'));
-      expect(submitSpy.receivedArguments()).toBeDefined()
+      expect(submitSpy).toHaveBeenCalled()
       expect(submitSpy.receivedArgument(0)[fieldName]).toEqual(value)
     });
 
