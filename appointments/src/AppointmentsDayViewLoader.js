8d50d8af8ffebaf4ef918358c3d8752b7d1bb975
Ezequiel <crespo.ep@gmail.com>
2022-02-02 17:28:32 -0300

    test for Appointments day view loader done

---
+++ appointments/src/AppointmentsDayViewLoader.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import { AppointmentsDayView } from './AppointmentsDayView'
+
+export const AppointmentsDayViewLoader = ({ today }) => {
+  useEffect(() => {
+    const from = today.setHours(0, 0, 0, 0);
+    const to = today.setHours(23, 59, 59, 999);
+
+    const fetchAppointments = () => {
+      window.fetch(`/appointments/${from}-${to}`, {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' }
+      })
+    }
+
+    fetchAppointments()
+  }, [])
+    // [from, to]
+  
+  return <AppointmentsDayView appointments={[]} />
+}
+
+AppointmentsDayViewLoader.defaultProps = {
+  today: new Date()
+}
