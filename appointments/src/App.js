d4cd655fb942b09159f7694085162054bd07521b
Ezequiel <crespo.ep@gmail.com>
2022-02-24 10:30:19 -0300

    hasta aqui llegue con shallow rendering, no me salio una prueba y decidi avanzar

--- appointments/src/App.js
+++ appointments/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { CustomerForm } from './CustomerForm'
 import { AppointmentsDayViewLoader } from './AppointmentsDayViewLoader'
 
 export const App = () => (
   <React.Fragment>
+    <div className="button-bar">
+      <button type="button" id="addCustomer">
+        Add customer and appointment
+      </button>
+    </div>
     <AppointmentsDayViewLoader />
   </React.Fragment>
 )
--- appointments/test/App.test.js
+++ appointments/test/App.test.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import {
+  id,
+  click,
+  childrenOf,
+  className,
   createShallowRenderer,
   type
 } from './shallowHelpers'
 import { App } from '../src/App'
 import { AppointmentsDayViewLoader } from '../src/AppointmentsDayViewLoader'
+import { CustomerForm } from '../src/CustomerForm'
 
 describe('App', () => {
-  let render, elementMatching
+  let render, elementMatching, child
 
   beforeEach(() => {
-    ({ render, elementMatching } = createShallowRenderer())
+    ({ render, elementMatching, child } = createShallowRenderer())
   })
 
   it('initially shows the AppointmentDayViewLoader', () => {
@@ -18,5 +23,28 @@ describe('App', () => {
     expect(
       elementMatching(type(AppointmentsDayViewLoader))
     ).toBeDefined()
+  })
+
+  it('has a button bar as the first child', () => {
+    render(<App />)
+    expect(child(0).type).toEqual('div')
+    expect(child(0).props.className).toEqual('button-bar')
+  })
+
+  it('has a button to initiate add customer and appointment action', () => {
+    render(<App />)
+    const buttons = childrenOf(elementMatching(className('button-bar')))
+    expect(buttons[0].type).toEqual('button')
+    expect(buttons[0].props.children).toEqual('Add customer and appointment')
+  })
+
+  const beginAddingCustomerAndAppointment = () => {
+    render(<App />)
+    click(elementMatching(id('addCustomer')))
+  }
+
+  it('displays the CustomerForm when button is clicked', async () => {
+    beginAddingCustomerAndAppointment()
+    expect(elementMatching(type(CustomerForm))).toBeDefined()
   })
 })
